8453b4d478d5f83e9051bb80e8bc823bdf7f42d3
Tasneem Farag <tasneemfarag@Mostafas-MacBook-Pro.local>
2022-11-27 12:22:56 -0500

    parsing the userInput

--- basics-02-added-scripts-imports/assets/scripts/app.js
+++ basics-02-added-scripts-imports/assets/scripts/app.js
@@ -1,1 +1,9 @@
-alert('This works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add() {
+    currentResult = currentResult + parseInt(userInput.value);
+    outputResult(currentResult, '');
+}
+
+addBtn.addEventListener('click', add);
